b071915e625216fbb02f6c005c399db81d3c63ae
Nico192008 <elengadot@gmail.com>
2025-01-27 09:11:41 +0800

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     content: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ' },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default : Date.now }
 });
 
 const Post = mongoose.model('Post', PostSchema);
