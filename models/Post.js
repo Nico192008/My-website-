1be749657a5d2863114ad3e489904f770013e59a
Nico192008 <elengadot@gmail.com>
2025-01-27 09:03:24 +0800

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
